e1a8a922fa1a778230ef424c0b08996ecc441a60
bibinwilson <bibinwilsonn@gmail.com>
2015-11-10 19:22:08 +0530

    disabling hostkey checking

--- script/deploy.sh
+++ script/deploy.sh
@@ -2,6 +2,6 @@
 
 echo "Starting the deploy"
 
-ssh ubuntu@52.11.240.13 -i ./script/mac-key.pem "sh /home/ubuntu/deploy.sh"
+ssh ubuntu@52.11.240.13 -i ~/.ssh/mac-key.pem -o StrictHostKeyChecking=no "sh /home/ubuntu/deploy.sh"
 
 echo "Successfull initiated chef client"
